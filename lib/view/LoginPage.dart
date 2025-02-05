7cc2f38dc06ee871a8197bac89a1bad0dee79f57
saheedalima <saheedalima318@gmail.com>
2025-02-06 00:46:00 +0530

    login page changed

--- lib/view/LoginPage.dart
+++ lib/view/LoginPage.dart
@@ -1,5 +1,4 @@
-import 'dart:io';
-
+// ignore_for_file: file_names
 import 'package:carcare/view/OtpScreen.dart';
 import 'package:flutter/material.dart';
 
@@ -24,14 +23,19 @@ class _LoginpageState extends State<Loginpage> {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              // App Icon
-              Icon(
-                Icons.local_car_wash, // Replace with your app icon
-                size: deviceHeight * .3,
-                color: Colors.yellow,
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.yellow,
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+                padding: const EdgeInsets.all(16.0),
+                child: Icon(
+                  Icons.local_car_wash,
+                  size: deviceHeight * 0.14,
+                  color: Colors.black,
+                ),
               ),
               SizedBox(height: deviceHeight * .05),
-              // App Name
               Text(
                 'Car Wash App',
                 style: TextStyle(
@@ -40,60 +44,65 @@ class _LoginpageState extends State<Loginpage> {
                   color: Colors.black,
                 ),
               ),
-              SizedBox(
-                  height: deviceHeight *
-                      .05), // Space between app name and login text
-              // Login Text
-              Text(
-                'Login',
-                style: TextStyle(
-                  fontSize: deviceHeight * .03,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.black,
-                ),
+              SizedBox(height: deviceHeight * .05),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: deviceHeight * .03,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox(height: deviceHeight * .003),
+                  Text(
+                    'Enter phone number to send one time \npassword',
+                    style: TextStyle(
+                      fontSize: deviceHeight * .02,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
               ),
+              SizedBox(height: deviceHeight * .03),
               SizedBox(
-                  height: deviceHeight * .03), // Space before the input field
-              // Mobile Number Input Field
-              SizedBox(
-                width: devicewidth * .05,
-                height: deviceHeight * .02,
+                width: devicewidth * .5,
+                height: deviceHeight * .07,
                 child: TextFormField(
                   keyboardType: TextInputType.phone,
                   decoration: const InputDecoration(
                     labelText: 'Mobile Number',
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.phone),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(30))
+                    ),
                   ),
                 ),
               ),
-              SizedBox(
-                  height: deviceHeight *
-                      .03), // Space between input field and button
-              // Send OTP Button
+              SizedBox(height: deviceHeight * .03),
               ElevatedButton(
                 onPressed: () {
-                  // Add your OTP sending logic here
                   Navigator.of(context)
                       .push(MaterialPageRoute(builder: (context) {
-                    return Otpscreen();
+                    return const Otpscreen();
                   }));
                 },
                 style: ElevatedButton.styleFrom(
-                  iconColor: Colors.yellow, // Background color
-                  shadowColor: Colors.black, // Text color
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 30, vertical: 15), // Padding
+                  backgroundColor: Colors.yellow,
+                  padding:
+                       EdgeInsets.symmetric(horizontal: devicewidth * .17, vertical: 13),
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30), // Rounded corners
+                    borderRadius: BorderRadius.circular(30),
                   ),
-                  elevation: 5,
                 ),
                 child: Text(
                   'Send OTP',
                   style: TextStyle(
-                    fontSize: deviceHeight * .03, // Font size
-                    fontWeight: FontWeight.bold, // Font weight
+                    fontSize: deviceHeight * .025,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black
                   ),
                 ),
               ),
